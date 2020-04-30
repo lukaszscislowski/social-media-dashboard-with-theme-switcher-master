6d5f6de3ea0dcb78df39ca918668a8540c64c6ad
lukaszscislowski <lukas.scislowski@gmail.com>
2020-04-30 20:21:34 +0200

    toggle module

--- script/script.js
+++ script/script.js
@@ -1,23 +1,14 @@
 'use strict' 
 
-// const toggle = document.querySelector('.toggle__label');
-// const body = document.querySelector('.dark');
-// toggle.addEventListener('click', () => {
-//     console.log('kliknięto klawisz');
-// });
+const toggle = document.querySelector('#toggle input');
 
-
-const toggle = document.querySelectorAll('label');
-const body = document.querySelectorAll('body');
-let colorTheme = '#1e202a'
-toggle.forEach((elem) =>{
-    elem.addEventListener('click', Theme);
-});
-
-function Theme(){
-    if(body.style.color === colorTheme)
-    body.style.color = '#ffff';
-    else
-    body.style.color = colorTheme;
-
+function Theme() {
+  if (this.checked) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  } else {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  }
 }
+toggle.addEventListener('click', Theme);
